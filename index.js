974b8e7c3b8b68febb4c458c986b389960b1e5e6
Tarun Kumar <tarunpandey2292@gmail.com>
2022-05-03 21:52:58 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function validate(){
+let firstName = document.getElementById('firstName').value
+let lastName = document.getElementById('lastName').value
+let email = document.getElementById('email').value
+let phoneNumber = document.getElementById('phoneNumber').value
+let state = document.getElementById('state').value
+let zipCode = document.getElementById('zipCode').value
+
+console.log(firstName,lastName,email,phoneNumber,state,zipCode)
+}
