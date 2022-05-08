b4eae2df39f3b71494e0ad29e689c4e0a0563a58
Tarun Kumar <tarunpandey2292@gmail.com>
2022-05-08 14:48:59 +0530

    Added states and setTouched()

--- index.js
+++ index.js
@@ -5,53 +5,91 @@ function validate() {
   let phoneNumber = document.getElementById("phoneNumber").value;
   let state = document.getElementById("state").value;
   let zipCode = document.getElementById("zipCode").value;
-  let tNCCheckBox = document.getElementById("tNCCheckBox").checked;
+  let tNCCheckBox = document.getElementById("tNCCheckBox").checked; //.checked is used with checkBox
 
-  if (firstName === "") {
-    document.getElementById("firstNameInvalid").style.display = "block";
-    document.getElementById("firstNameValid").style.display = "none";
-  } else {
-    document.getElementById("firstNameValid").style.display = "block";
-    document.getElementById("firstNameInvalid").style.display = "none";
-  }
-
-  if (lastName === "") {
-    document.getElementById("lastNameInvalid").style.display = "block";
-    document.getElementById("lastNameValid").style.display = "none";
-  } else {
-    document.getElementById("lastNameValid").style.display = "block";
-    document.getElementById("lastNameInvalid").style.display = "none";
+  if (fieldNamesTouched["firstName"]) {
+    if (firstName === "") {
+      document.getElementById("firstNameInvalid").style.display = "block";
+      document.getElementById("firstNameValid").style.display = "none";
+    } else {
+      document.getElementById("firstNameValid").style.display = "block";
+      document.getElementById("firstNameInvalid").style.display = "none";
+    }
   }
 
-  if (email === "" || !email.includes('@') || email.startsWith('@') || !email.includes('.') || email.slice(email.lastIndexOf('.')).length < 3) {
-    document.getElementById("emailInvalid").style.display = "block";
-    document.getElementById("emailValid").style.display = "none";
-  } else {
-    document.getElementById("emailValid").style.display = "block";
-    document.getElementById("emailInvalid").style.display = "none";
+  if (fieldNamesTouched["lastName"]) {
+    if (lastName === "") {
+      document.getElementById("lastNameInvalid").style.display = "block";
+      document.getElementById("lastNameValid").style.display = "none";
+    } else {
+      document.getElementById("lastNameValid").style.display = "block";
+      document.getElementById("lastNameInvalid").style.display = "none";
+    }
   }
 
-  if (phoneNumber === "" || phoneNumber.length != 10 || Number(phoneNumber[0]) <6) {
-    document.getElementById("phoneInvalid").style.display = "block";
-    document.getElementById("phoneValid").style.display = "none";
-  } else {
-    document.getElementById("phoneValid").style.display = "block";
-    document.getElementById("phoneInvalid").style.display = "none";
+  if (fieldNamesTouched["email"]) {
+    if (
+      email === "" ||
+      !email.includes("@") ||
+      email.startsWith("@") ||
+      !email.includes(".") ||
+      email.slice(email.lastIndexOf(".")).length < 3
+    ) {
+      document.getElementById("emailInvalid").style.display = "block";
+      document.getElementById("emailValid").style.display = "none";
+    } else {
+      document.getElementById("emailValid").style.display = "block";
+      document.getElementById("emailInvalid").style.display = "none";
+    }
   }
 
-  if (zipCode === "" || zipCode.length != 6) {
-    document.getElementById("zipInvalid").style.display = "block";
-    document.getElementById("zipValid").style.display = "none";
+  if (fieldNamesTouched["phoneNumber"]) {
+    if (
+      phoneNumber === "" ||
+      phoneNumber.length != 10 ||
+      Number(phoneNumber[0]) < 6
+    ) {
+      document.getElementById("phoneInvalid").style.display = "block";
+      document.getElementById("phoneValid").style.display = "none";
+    } else {
+      document.getElementById("phoneValid").style.display = "block";
+      document.getElementById("phoneInvalid").style.display = "none";
+    }
+  }
+
+  if (fieldNamesTouched["zipCode"]) {
+    if (zipCode === "" || zipCode.length != 6) {
+      document.getElementById("zipInvalid").style.display = "block";
+      document.getElementById("zipValid").style.display = "none";
+    } else {
+      document.getElementById("zipValid").style.display = "block";
+      document.getElementById("zipInvalid").style.display = "none";
+    }
+  }
+
+  if (!tNCCheckBox) {
+    document.getElementById("tNCInvalid").style.display = "block";
   } else {
-    document.getElementById("zipValid").style.display = "block";
-    document.getElementById("zipInvalid").style.display = "none";
+    document.getElementById("tNCInvalid").style.display = "none";
   }
-  
-  if(!tNCCheckBox){
-   tNCInvalid.style.display = 'block'
+
+  if (state === "") {
+    document.getElementById("stateInvalid").style.display = "block";
+    document.getElementById("stateValid").style.display = "none";
+  } else {
+    document.getElementById("stateValid").style.display = "block";
+    document.getElementById("stateInvalid").style.display = "none";
   }
-  else{
-    tNCInvalid.style.display = ''
-  }
-  
+}
+
+let fieldNamesTouched = {
+  firstName: false,
+  lastName: false,
+  email: false,
+  phoneNumber: false,
+  zipCode: false,
+};
+function setTouched(fieldName) {
+  fieldNamesTouched[fieldName] = true;
+  validate();
 }
